e885e73ba265627018bc5a4b4591e77588e62744
Tavia Sterling <TaviaSterling@outlook.com>
2018-08-06 13:46:07 -0600

    create Alligator and Beaver

--- app/models/Beaver.js
+++ app/models/Beaver.js
@@ -1,10 +1,11 @@
-class Alligator {
-    constructor(name, color, gender, weight) {
+class Beaver {
+    constructor(name,furColor,gender,weight) {
         this.id = Math.random()
         this.name = name || ''
-        this.color = color || ''
+        this.furColor = furColor || ''
         this.gender = gender || ''
         this.weight = weight || 0
     }
 }
-export default Alligator
+
+export default Beaver

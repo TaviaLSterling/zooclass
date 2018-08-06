06d036fdc56935c969a82ed9c2385e0d57f9e2ab
Tavia Sterling <TaviaSterling@outlook.com>
2018-08-06 13:48:42 -0600

    create Cow class

---
+++ app/models/Cow.js
@@ -0,0 +1,10 @@
+class Cow {
+    constructor(name,furColor,gender,weight) {
+        this.id = Math.random()
+        this.name = name || ''
+        this.furColor = furColor || ''
+        this.gender = gender || ''
+        this.weight = weight || 0
+    }
+}
+export default Cow

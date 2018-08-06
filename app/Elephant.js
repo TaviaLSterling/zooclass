112b5ecd24d8d95182b20e525821fc9ff324f7f1
Tavia Sterling <TaviaSterling@outlook.com>
2018-08-06 13:54:17 -0600

    Flamingo and Elephant classes

---
+++ app/Elephant.js
@@ -0,0 +1,10 @@
+class Elephant {
+    constructor(name,color,gender,weight) {
+        this.id = Math.random()
+        this.name = name || ''
+        this.color = color || ''
+        this.gender = gender || ''
+        this.weight = weight || 0
+    }
+}
+export default Elephant
---
+++ app/Flamingo.js
@@ -0,0 +1,11 @@
+class Flamingo {
+    constructor(name,color,gender,weight,height) {
+        this.id = Math.random()
+        this.name = name || ''
+        this.color = color || ''
+        this.gender = gender || ''
+        this.weight = weight || 0
+        this.height = height || 0
+        }
+}
+export default Flamingo
